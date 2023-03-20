52c4ee4955cb67b519b8f37950929fc1707376fa
FabianUlises <ulisesfab.car@outlook.com>
2023-03-20 08:15:40 -0700

    Added some features to todo list component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
+    // State
+    const [ input, setInput] = useState('');
   return (
     <div>
         <h2>Todo List</h2>
         <form>
-            <input type='text' placeholder='enter todo item' />
+            <input type='text' placeholder='enter todo item'
+                onChange={(e) => 
+                    setInput(e.target.value)
+                }
+            />
             <button type='submit'>Submit</button>
         </form>
         <ul id='todo-list'>
 
         </ul>
+        <button type='button'>Clear</button>
     </div>
   )
 }
