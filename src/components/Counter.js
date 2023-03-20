90150582f0c2cbf7174e24d493135d34d5dd3cb7
FabianUlises <ulisesfab.car@outlook.com>
2023-03-20 07:58:21 -0700

    Updated form to update state via user input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from './../features/counterSlice';
+import { decrement, increment, incrementByAmount } from './../features/counterSlice';
 
 const Counter = () => {
+  // State
+  const [ input, setInput ] = useState(0);
   // Get state from redux store
   const count = useSelector((state) => state.value)
   // useDispatch hook
   const dispatch = useDispatch();
+  // Handle form submit
+  const byAmountSubmit = (e) => {
+    e.preventDefault();
+    if(!Number(input)) return
+    dispatch(incrementByAmount(Number(input)));
+    document.querySelector('#inputValue').value = '';
+  };
   return (
     <div>
       <h1 className='counter-state'>{count}</h1>
       <button id='addCounter' type='button' onClick={() => dispatch(increment())}>Increment</button>
       <button id='subCounter' type='button' onClick={() => dispatch(decrement())}>Decrement</button>
-      <form className='counter-form'>
-        <input type='text' placeholder='enter value' id='inputValue'/>
-        <button type='submit' onClick={(e) =>{
-          e.preventDefault();
-          const text = document.querySelector('#inputValue').value;
-          console.log(text);
-        } }>Submit</button>
+      <form className='counter-form' onSubmit={(e) => byAmountSubmit(e)}>
+        <input type='text' placeholder='enter value' id='inputValue' onChange={(e) => setInput(e.target.value)} />
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
